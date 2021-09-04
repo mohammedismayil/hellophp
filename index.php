beceb9707ff589c50d772107ac28e78bfab58632
ismayil <mohammedismayil10@gmail.com>
2021-09-04 19:25:29 +0530

    adding files

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="containerdiv flex">
+
+            <h2 class="containertitle">Food Delivery</h2>
+            <h3 class="containersubtitle">When It Comes To Delivery, The First And Foremost Preferred Delivery Service Of The Customer Is Food. Facilitate Food Delivery Services From The Nearby</h3>
+            <!-- <h2 class="containertitle">Food Delivery</h2> -->
+
+
+        </div>
+        <div class="containerimgdiv flex">
+            <img src="./Images/biryani.jpeg" class="containerimg">
+
+        </div>
+
+
+
+
+
+
+
+    </div>
+</body>
+
+</html>
